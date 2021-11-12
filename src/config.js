8a0059736ba6f7bd40c2d58371f2538eca0fca80
Atharva-gt <guptaatharva098@gmail.com>
2021-11-12 12:10:31 +0530

    u

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const app_config = {
-    api_url: 'http://localhost:5000',
-    // api_url:'https://fileshare-backend1.herokuapp.com/'
+    // api_url: 'http://localhost:5000',
+    api_url:'https://fileshare-backend1.herokuapp.com/'
 }
 
 export default app_config;
